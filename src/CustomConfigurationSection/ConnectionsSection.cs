1337ce9e8c2004392df8a6dd3daf2f4f63ea13a8
Raphael A. F. Cardoso <ferronicardoso@outlook.com>
2020-12-22 23:41:53 -0300

    Projeto de exemplo do uso do ConfigurationSection

---
+++ src/CustomConfigurationSection/ConnectionsSection.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace CustomConfigurationSection
+{
+    public class ConnectionsSection : ConfigurationSection
+    {
+        [ConfigurationProperty("", IsRequired = true, IsDefaultCollection = true)]
+        public ConnectionsInstanceCollection Instances
+        {
+            get { return (ConnectionsInstanceCollection)this[""]; }
+            set { this[""] = value; }
+        }
+
+        public List<ConnectionsInstanceElement> ToList()
+        {
+            var list = new List<ConnectionsInstanceElement>();
+            foreach (ConnectionsInstanceElement item in this.Instances)
+            {
+                list.Add(item);
+            }
+            return list;
+        }
+    }
+
+    public class ConnectionsInstanceCollection : ConfigurationElementCollection
+    {
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new ConnectionsInstanceElement();
+        }
+
+        protected override object GetElementKey(ConfigurationElement element)
+        {
+            return ((ConnectionsInstanceElement)element).Name;
+        }
+    }
+
+    public class ConnectionsInstanceElement : ConfigurationElement
+    {
+        [ConfigurationProperty("name", IsKey = true, IsRequired = true)]
+        public string Name
+        {
+            get { return (string)base["name"]; }
+            set { base["name"] = value; }
+        }
+
+        [ConfigurationProperty("value", IsRequired = true)]
+        public string @Value
+        {
+            get { return (string)base["value"]; }
+            set { base["value"] = value; }
+        }
+
+        [ConfigurationProperty("type", IsRequired = true)]
+        public DataSource @Type
+        {
+            get { return (DataSource)base["type"]; }
+            set { base["type"] = value; }
+        }
+    }
+
+    public enum DataSource
+    {
+        Firebird,
+        MySql,
+        Oracle,
+        PostgreSql,
+        Progress,
+        Protheus,
+        Sap,
+        SqlLite,
+        SqlServer
+    }
+}
---
+++ src/CustomConfigurationSection/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomConfigurationSection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CreateSection();
+
+            GetSection();
+            Console.ReadKey();
+        }
+
+        static void CreateSection()
+        {
+            try
+            {
+
+                ConnectionsSection customSection;
+
+                // Get the current configuration file.
+                System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                // Create the section entry  
+                // in the <configSections> and the 
+                // related target section in <configuration>.
+                if (config.Sections["ConnectionsSection"] == null)
+                {
+                    customSection = new ConnectionsSection();
+
+                    config.Sections.Add("ConnectionsSection", customSection);
+                    customSection.SectionInformation.ForceSave = true;
+                    config.Save(ConfigurationSaveMode.Full);
+                }
+            }
+            catch (ConfigurationErrorsException err)
+            {
+                Console.WriteLine(err.ToString());
+            }
+        }
+
+        static void GetSection()
+        {
+
+            try
+            {
+                var connections = ConfigurationManager.GetSection("ConnectionsSection") as ConnectionsSection;
+
+                if (connections != null)
+                { 
+                    connections.ToList().ForEach(item =>
+                    {
+                        Console.WriteLine("Name: {0}", item.Name);
+                        Console.WriteLine("Type: {0}", item.Type);
+                        Console.WriteLine("Value: {0}", item.Value);
+                        Console.WriteLine("--------------------------------------------------------------------------------------------------------");
+                    });
+                }
+                else
+                {
+                    Console.WriteLine("Failed to load CustomSection.");
+                }
+            }
+            catch (ConfigurationErrorsException err)
+            {
+                Console.WriteLine(err.ToString());
+            }
+        }
+    }
+}
